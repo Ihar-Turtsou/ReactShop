ed93ef2a659b1de6ea390c74155f989daee8ffab
Igtur <ig.turtsov@gmail.com>
2023-07-13 20:48:59 +0400

    Add sceleton and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import './scss/app.scss'
+import { Header } from "./components/Header";
+import { Categories } from "./components/Categories";
+import { Sort } from "./components/Sort";
+import { PizzBlock } from "./components/PizzaBlock";
+
 
 function App() {
   return (
-    <div className="App">
-    hello
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzBlock title="Мексиканская" price={250}/>
+            <PizzBlock />
+            <PizzBlock />
+            <PizzBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
